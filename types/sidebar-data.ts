784e4d4f7719675776d00338ca443482f28e4fda
SignalTheWolf <signalthecomputercanine@gmail.com>
2024-07-23 12:44:32 -0400

    Updated sidebar-data.ts

--- types/sidebar-data.ts
+++ types/sidebar-data.ts
@@ -3,19 +3,19 @@ import { Tables } from "@/supabase/types"
 export type DataListType =
   | Tables<"collections">[]
   | Tables<"chats">[]
-  /*| Tables<"presets">[]
+  |Tables<"presets">[]
   | Tables<"prompts">[]
   | Tables<"files">[]
   | Tables<"assistants">[]
   | Tables<"tools">[]
-  | Tables<"models">[] */
+  | Tables<"models">[]
 
 export type DataItemType =
   | Tables<"collections">
   | Tables<"chats">
-  | /* Tables<"presets">
+  | Tables<"presets">
   | Tables<"prompts">
   | Tables<"files">
   | Tables<"assistants">
   | Tables<"tools">
-  | Tables<"models"> */
+  | Tables<"models">

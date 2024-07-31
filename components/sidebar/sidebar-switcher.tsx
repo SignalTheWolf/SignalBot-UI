ca76b58996a2cc7072ca25b41e2c28114261c1f0
SignalTheWolf <signalthecomputercanine@gmail.com>
2024-07-31 15:08:42 -0400

    made available icons in sidebar based on role

--- components/sidebar/sidebar-switcher.tsx
+++ components/sidebar/sidebar-switcher.tsx
@@ -41,7 +41,7 @@ export const SidebarSwitcher: FC<SidebarSwitcherProps> = ({
         if (error) {
           console.error('Error fetching user role:', error.message)
         } else if (data) {
-          setIsAdmin(data.isAdmin)
+          setIsAdmin(data.Is_Admin)
         }
       }
     }
